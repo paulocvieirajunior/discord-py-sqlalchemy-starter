751998ba46e039322c5eaa1c58bf81b8e35b9311
Paulo Vieira <paulocvieirajunior@gmail.com>
2026-04-03 23:30:29 -0300

    feat: add bot intents

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-from app.core import settings
+from core import settings
+from discord import Intents
 from discord.ext.commands import Bot
 
 class Client(Bot):
     async def setup_hook(self):
         pass
 
-client = Client()
+intents = Intents.all()
+client = Client(command_prefix="$", intents=intents)
+
 client.run(settings.BOT_TOKEN)
